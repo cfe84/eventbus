3fad6c778dfa23d78c840c7073dfe50391d9484e
Charles Feval <chfev@CharlesFs-MacBook-Pro.local>
2016-07-04 18:34:38 -0700

    Generating package.json

--- publish.sh
+++ publish.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+VERSION=`cat version.txt`
+REVISION=`git describe | sed -E s/[^-]*-\([0-9]+\)-.*$/\1/g`
+
+cat package.json.template | sed s/%VERSION%/$VERSION.$REVISION/g > bin/package.json
+
 cd bin
 npm publish
